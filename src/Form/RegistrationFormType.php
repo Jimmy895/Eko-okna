ad2cc20e0bb3cb8f3410d981b420578c785def0a
GodonPa <linkinpata@gmail.com>
2023-03-01 16:56:09 +0100

    Type - przerobiono automatycznie wygenerowany formularz zgodnie z założeniami projektu

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,7 +5,9 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,23 +19,57 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('login')
+            ->add('login', TextType::class, [
+                'required' => true,
+                'label' => 'Login:',
+                'attr' => [
+                    'class' => 'form-control border border-1',
+                ],
+                'row_attr' => [
+                    'class' => 'testowy parent',
+                ],
+                'label_attr' => [
+                    'class' => 'h5',
+                ],
+            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control border border-1',
+                    'autocomplete' => 'new-password'
+                ],
+                'label_attr' => [
+                    'class' => 'h5',
+                ],
+
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Twoje hasło powinno zawierać co najmniej {{ limit }} znaków',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
+            ])
+            ->add('storage_list_id', ChoiceType::class, [
+                'choices'  => $options['storages'],
+                'label' => 'Przypisz magazyn:',
+                'row_attr' => [
+                    'class' => 'd-flex justify-content-between align-items-center',
+                ],
+                'attr' => [
+                    'class' => 'btn h5 px-3 py-2 bg-white rounded-2 border border-primary',
+                ],
+                'label_attr' => [
+                    'class' => 'h5',
+                ],
+
             ])
         ;
     }
@@ -41,6 +77,7 @@ class RegistrationFormType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
+            'storages' => null,
             'data_class' => User::class,
         ]);
     }
