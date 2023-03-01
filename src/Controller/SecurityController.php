a406ef4b71bedfb8b2c9cf9a8366357c32631cfd
GodonPa <linkinpata@gmail.com>
2023-03-01 14:49:10 +0100

    Controller - zmieniono ścieżki

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,11 +9,11 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
+    #[Route(path: '/', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
          if ($this->getUser()) {
-             return $this->redirectToRoute('app');
+             return $this->redirectToRoute('app_dashboard');
          }
 
         // get the login error if there is one
