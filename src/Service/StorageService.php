a93617deadb1aed813d59e9a1e2dc1ac60a4e1d5
GodonPa <linkinpata@gmail.com>
2023-03-01 14:46:14 +0100

    Service - ustanowienie połączenia między plikami serwisu i repository - stworzenie funkcji odpowiedzialnych za obróbkę danych wyciągniętych z bazy na potrzeby odpowieniej prezentacji danych w formularzu tworzenia magazynu

---
+++ src/Service/StorageService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\StorageRepository;
+
+class StorageService
+{
+    private StorageRepository $storageRepo;
+
+    public function __construct(StorageRepository $storageRepo) {
+        $this->storageRepo = $storageRepo;
+    }
+
+    public function prepareUsersArrayForSelect() {
+        $users = $this->storageRepo->selectAllUsers();
+        $usersForSelect = [];
+
+        foreach ($users as $user) {
+            $usersForSelect[$user['login']] = $user['id'];
+        }
+
+        return $usersForSelect;
+    }
+
+    public function insertNewStorage(array $data) {
+        $lastStorageId = $this->storageRepo->insertNewStorage($data['name']);
+        $this->storageRepo->updateUserStorage($data['employee'], $lastStorageId);
+    }
+}
