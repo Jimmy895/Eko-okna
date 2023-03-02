7bd43b0acb8995a8df2bacec45587ed2b2fac25a
GodonPa <linkinpata@gmail.com>
2023-03-02 22:53:13 +0100

    Type - zmodyfikowano formularz tworzenia magazynów tak aby obsługiwał również ich edycję - dodano instrukcje warunkowe, które sprawdzają i renderują odpowiedni content zależnie od sytuacji

--- src/Form/CreateNewStorageType.php
+++ src/Form/CreateNewStorageType.php
@@ -3,8 +3,11 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -23,31 +26,43 @@ class CreateNewStorageType extends AbstractType
                 'label_attr' => [
                     'class' => 'h5',
                 ],
-            ])
-            ->add('employee',ChoiceType::class, [
-                'multiple' => true,
-                'choices' => $options['employee'],
-                'required' => true,
-                'label' => 'Przypisz pracowników:',
-                'row_attr' => [
-                    'class' => 'd-flex justify-content-between align-items-center',
-                ],
-                'attr' => [
-                    'class' => 'selectpicker',
+            ]);
+
+        if ($options['create']) {
+            $builder
+                ->add('employee', ChoiceType::class, [
                     'multiple' => true,
-                    'data-live-search' => true
+                    'choices' => $options['employee'],
+                    'required' => true,
+                    'label' => 'Przypisz pracowników:',
+                    'row_attr' => [
+                        'class' => 'd-flex justify-content-between align-items-center',
+                    ],
+                    'attr' => [
+                        'class' => 'selectpicker',
+                        'multiple' => true,
+                        'data-live-search' => true
+                    ],
+                    'label_attr' => [
+                        'class' => 'h5',
+                    ],
+                ]);
+        }
+
+        $builder
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-lg btn-outline-primary',
                 ],
-                'label_attr' => [
-                    'class' => 'h5',
-                ],
-            ])
-        ;
+                'label' => is_null($options['create']) ? 'Edytuj magazyn' : 'Stwórz magazyn'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'employee' => null,
+            'create' => null,
         ]);
     }
 }
