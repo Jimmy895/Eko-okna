2d0c51a5f44c07c8b44515a60d416a8f834a7a29
GodonPa <linkinpata@gmail.com>
2023-03-05 15:02:53 +0100

    Type - utworzono szablon formularza przyjęcia towaru przy pomocy form buildera, stworzono pola: - select - wybór nazwy produktu wcześniej stworzonego przez administratora w sekcji tworzenia artykułu, dla poprawy czytelności w select wyświetlono również jendostkę miary artykułu, - kod artykułu - number, - liczebność artykułu - number, - stawka vat - number, - cena - number, - opcjonalne załączenie pliku PDF - fileType

---
+++ src/Form/ArticleEntryType.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleEntryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('article',ChoiceType::class, [
+
+                'choices' => $options['article'],
+                'required' => true,
+                'label' => 'Nazwa artykułu:',
+                'row_attr' => [
+                    'class' => 'd-flex flex-column',
+                ],
+                'attr' => [
+                    'class' => 'btn text-left h5 px-3 py-2 bg-white rounded-2 border border-primary',
+                ],
+                'label_attr' => [
+                    'class' => 'h5',
+                ],
+            ])
+            ->add('code', NumberType::class, [
+                'required' => true,
+                'label' => 'Kod artykułu:',
+                'attr' => [
+                    'class' => 'form-control border border-1',
+                    'placeholder' => 'Kod artykułu..'
+                ],
+                'label_attr' => [
+                    'class' => 'h5',
+                ],
+            ])
+            ->add('amount', NumberType::class, [
+                'required' => true,
+                'label' => 'Ilość przyjęta:',
+                'attr' => [
+                    'class' => 'form-control border border-1',
+                    'placeholder' => 'Ilość..'
+                ],
+                'label_attr' => [
+                    'class' => 'h5',
+                ],
+            ])
+            ->add('vat', NumberType::class, [
+                'required' => true,
+                'label' => 'Stawka VAT:',
+                'attr' => [
+                    'class' => 'form-control border border-1',
+                    'placeholder' => 'VAT..'
+                ],
+                'label_attr' => [
+                    'class' => 'h5',
+                ],
+            ])
+            ->add('price', NumberType::class, [
+                'required' => true,
+                'label' => 'Cena jednostkowa:',
+                'attr' => [
+                    'class' => 'form-control border border-1',
+                    'placeholder' => 'Cena bez podatku..'
+                ],
+                'label_attr' => [
+                    'class' => 'h5',
+                ],
+            ])
+            ->add('attachment', FileType::class, [
+                'mapped' => false,
+                'multiple' => false,
+                'required' => false,
+                'label' => 'Faktura *PDF (opcjonalnie):',
+                'attr' => [
+                    'class' => 'form-control border border-1',
+                ],
+                'label_attr' => [
+                    'class' => 'h5',
+                ],
+                'constraints' => [
+
+                    new File([
+                        'maxSize' => '1024m',
+                        'mimeTypes' => [
+                            'application/pdf',
+                            'application/x-pdf',
+                        ],
+                        'mimeTypesMessage' => 'Proszę przesłać plik PDF',
+                    ])
+
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'article' => null,
+            'data_class' => null
+        ]);
+    }
+}
